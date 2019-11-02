53b4226f8dc31d09818e0deef719acd81d765126
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2019-11-02 19:20:54 +1100

    Added cloud function class for constructing calls to cloud functions

---
+++ src/cloud_function/cloud_function.py
@@ -0,0 +1,72 @@
+# cloud_function.py
+
+# Author : aarontillekeratne
+# Date : 2/11/19
+
+# This file is part of trapezitam.
+
+# trapezitam is free software:
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+
+# trapezitam is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with trapezitam.  
+# If not, see <https://www.gnu.org/licenses/>.
+
+import requests
+
+TOKEN_REQUEST_HEADER = {'Metadata-Flavour': 'Google'}
+
+
+class CloudFunctionFactory:
+
+    def __init__(self, metadata_server_token_url):
+        self.metadata_server_toke_url = metadata_server_token_url
+
+    def create_function(self, function_url):
+        """
+        Creates a new cloud function object.
+        :param function_url: Url to the cloud function.
+        :return:
+        """
+        return _CloudFunction(function_url, self.metadata_server_toke_url)
+
+
+class _CloudFunction:
+
+    def __init__(self, function_url, metadata_server_token_url):
+        self.func_url = function_url
+        self.metadata_server_token_url = metadata_server_token_url
+
+    def _create_auth_header(self):
+        """
+        Creates the authorisation header.
+        :return:
+        """
+        token_request_url = self.metadata_server_token_url + self.func_url
+        token_request_headers = TOKEN_REQUEST_HEADER
+
+        token_response = requests.get(token_request_url,
+                                      headers=token_request_headers)
+        jwt = token_response.content.decode('utf-8')
+
+        return {"Authorization": "bearer {0}".format(jwt)}
+
+    def __call__(self, payload, **kwargs):
+        """
+        Calls the cloud function
+        :param args:
+        :param payload:
+        :param kwargs:
+        :return:
+        """
+        auth_header = self._create_auth_header()
+
+        return requests.post(self.func_url, headers=auth_header,
+                             json=payload)
